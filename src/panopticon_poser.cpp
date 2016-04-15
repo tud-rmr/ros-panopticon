8678d851cb17f51485404209c78a2f4414920827
roboterlabor <raultron@gmail.com>
2016-04-15 12:17:36 +0200

    Pose works a bit

--- src/panopticon_poser.cpp
+++ src/panopticon_poser.cpp
@@ -11,15 +11,15 @@ namespace rmr {
 PanopticonPoser::PanopticonPoser(ros::NodeHandle _nh) {
   nh = _nh;
 
-  subPoseCam0.subscribe(nh, "camera0/pose", 100);
-  subPoseCam1.subscribe(nh, "camera1/pose", 100);
-  subPoseCam2.subscribe(nh, "camera2/pose", 100);
-  subPoseCam3.subscribe(nh, "camera3/pose", 100);
+  subPoseCam0.subscribe(nh, "camera0/pose", 10);
+  subPoseCam1.subscribe(nh, "camera1/pose", 10);
+  subPoseCam2.subscribe(nh, "camera2/pose", 10);
+  subPoseCam3.subscribe(nh, "camera3/pose", 10);
 
-  tfFilterPoseCam0 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam0, tfListener, "usb_cam0", 100);
-  tfFilterPoseCam1 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam1, tfListener, "usb_cam1", 100);
-  tfFilterPoseCam2 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam2, tfListener, "usb_cam2", 100);
-  tfFilterPoseCam3 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam3, tfListener, "usb_cam3", 100);
+  tfFilterPoseCam0 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam0, tfListener, "usb_cam0", 10);
+  tfFilterPoseCam1 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam1, tfListener, "usb_cam1", 10);
+  tfFilterPoseCam2 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam2, tfListener, "usb_cam2", 10);
+  tfFilterPoseCam3 = new tf::MessageFilter<geometry_msgs::PoseStamped>(subPoseCam3, tfListener, "usb_cam3", 10);
 
   tfFilterPoseCam0->registerCallback(&PanopticonPoser::cameraPoseCallback, this);
   tfFilterPoseCam1->registerCallback(&PanopticonPoser::cameraPoseCallback, this);
@@ -41,11 +41,15 @@ void PanopticonPoser::cameraPoseCallback(const geometry_msgs::PoseStamped::Const
 
   if (frameName != "map" && frameName != "center") {
     try{
-      geometry_msgs::PoseStamped markerInWorldMsg;
-      tfListener.waitForTransform("map", frameName, ros::Time(0), ros::Duration(3.0));
-      tfListener.transformPose("map", ros::Time::now(), *msg, "map", markerInWorldMsg);
-      geometry_msgs::PoseWithCovarianceStamped poseWithCovMsg = poseToPoseWithCovariance(*msg);
-      pubPose.publish(poseWithCovMsg);    
+      geometry_msgs::PoseStamped pose(*msg);
+      geometry_msgs::PoseStamped poseInWorld;
+
+      pose.header.stamp = poseInWorld.header.stamp = ros::Time(0);
+
+      // tfListener.waitForTransform("map", frameName, ros::Time(0), ros::Duration(3.0));
+      tfListener.transformPose("map", pose, poseInWorld);
+      geometry_msgs::PoseWithCovarianceStamped poseWorldWithCov = poseToPoseWithCovariance(poseInWorld);
+      pubPose.publish(poseWorldWithCov);    
     } catch (tf::TransformException ex) {
       ROS_ERROR("%s",ex.what());
     }
